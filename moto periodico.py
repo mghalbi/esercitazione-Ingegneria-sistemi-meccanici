b7109acbf741bd2d2c54671548b95530be7dc3ff
mohsen <mohsen.ghalbi.consulente@infocert.it>
2020-10-16 19:27:22 +0200

    Finished moto periodico

--- moto periodico.py
+++ moto periodico.py
@@ -40,33 +40,50 @@ dphi = np.pi/100
 vphia = np.arange(0,np.pi,dphi)
 vphim = np.arange(np.pi, 2*np.pi, dphi)
 vphi =  np.concatenate((vphia, vphim), axis=None)
+vhpigrad = vphi*18*np.pi
 vFa = Fa*np.ones(len(vphia))
 vFm = Fm*np.ones(len(vphim))
 vFr = np.concatenate((vFa, vFm), axis=None)
 taum = -r*np.sin(vphi)
 vMr = vFr*np.array(taum).transpose()
 
+#Andamento del momento resistente ridotto
+plt.plot(vhpigrad,vMr, label='$M_r^* [Nm]$')
+
+plt.xlabel(r'$\varphi [°]$')
+plt.ylabel('$M_r^*$ [Nm]')
+plt.grid()
+plt.show()
+
 # coppia d'inerzia
 vMi = -m*pow(r,2)*pow(w,2)*np.array(np.sin(vphi))*np.array(np.cos(vphi)).transpose()
 # coppia totale
 vMmrid = Mmrid*np.ones(len(vphi))
 vMtot = vMmrid + vMr +vMi
-plt.plot(vphi,vMtot)
-plt.plot(vphi,vMmrid)
-plt.plot(vphi,vMi)
-plt.plot(vphi,vMr)
+plt.plot(vhpigrad,vMmrid, label='$M_m^* [Nm]$')
+plt.plot(vhpigrad,vMi, label='$M_i^* [Nm]$')
+plt.plot(vhpigrad,vMr, label='$M_r^* [Nm]$')
+plt.legend(loc="lower left")
+plt.xlabel(r'$\varphi [°]$')
+plt.ylabel('Nm')
+plt.grid()
 plt.show()
 
 vE=integrate.cumtrapz(vMtot,vphi, initial=0)
 
-plt.plot(vphi,vE)
-plt.plot(vphi,vMtot)
+plt.plot(vhpigrad,vE,label='$E_r$')
+plt.plot(vhpigrad,vMtot,label='$M_m^*+M_r^*+M_i^* [Nm]$')
+plt.legend(loc="lower left")
+plt.xlabel(r'$\varphi [°]$')
+plt.title('Andamento di $M^∗$ e di $E_r$')
+plt.grid()
+fig = plt.gcf()
+fig.canvas.set_window_title('Andamento di $M^∗$ e di $E_r$')
 plt.show()
 
 DEmax=np.max(vE)-np.min(vE)
 Jtrid=DEmax/(i*pow(w,2))
 Jv=Jtrid*pow(tau,2)/eta-Jm
-print(['Jv [kg*m^2]=', Jv])
 Jvrid=Jv/pow(tau,2)*eta;
 Jmrid=Jm/pow(tau,2)*eta;
 dt=0.01; tf=30;
@@ -78,13 +95,18 @@ for j in np.arange(0,len(t)):
 	Mmrid = motore(phip[j])
 	phig = 2*np.pi*(phi[j]/(2*np.pi)-np.fix(phi[j]/(2*np.pi)))
 	Mr = np.interp(phig,vphi,vMr)	
-	print((Jvrid+Jmrid+m*pow(r,2)*pow(np.sin(phi[j]),2)))
 	phipp=(Mmrid+Mr-m*pow(r,2)*pow(phip[j],2)*np.sin(phi[j])*np.cos(phi[j]))/(Jvrid+Jmrid+m*pow(r,2)*pow(np.sin(phi[j]),2))
 	phip.append(phip[j]+phipp*dt)
 	phi.append(phi[j]+phip[j]*dt)
 
 
 plt.plot(t,np.array(phip[:-1])/(2*np.pi/60))
+plt.grid()
+plt.ylabel('n [rpm]')
+plt.xlabel('tempo [s]')
+fig = plt.gcf()
+plt.title('Andamento della velocità dell\'albero di manovella')
+fig.canvas.set_window_title("Andamento della velocità dell'albero di manovella")
 plt.show()
 
 
